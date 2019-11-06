cf7871a5b0240acd39249c2a45eac29cf99f345f
Rustymattok <you@example.com>
2019-11-06 08:24:23 +0300

    обновленно в соответсвии с требованиями робота

--- pprint_json.py
+++ pprint_json.py
@@ -2,29 +2,23 @@ import json
 
 
 def load_data(filepath):
-    """
-    Open json_file and convert it to the json_list.
-    """
     try:
+        if filepath[-5:len(filepath)] != '.json':  # for format .json
+            return 'not correct format'
         with open(filepath) as f:
             json_string = f.read()
             json_list = json.loads(json_string)
     except FileNotFoundError:
         return 'not correct way for File'
-    except Exception:
-        return 'not correct format'
     return json_list
 
 
 def pretty_print_json(json_list):
-    """
-    Convert json_list to readable format of json.
-    """
-    json_format = json.dumps(json_list, ensure_ascii=False, indent=4)
-    return json_format
+    json_visual = json.dumps(json_list, ensure_ascii=False, indent=4)
+    return json_visual  # convert json_list to more readable format in console.
 
 
 if __name__ == '__main__':
     filepath = input('enter file way: ')
-    result = pretty_print_json(load_data(filepath))
-    print(result)
+    json_visual = pretty_print_json(load_data(filepath))
+    print(json_visual)
