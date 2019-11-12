18ac75542f044c236f21f6145d929a4190169018
Rustymattok <you@example.com>
2019-11-12 19:49:44 +0300

    update name parameters and properties of args

--- pprint_json.py
+++ pprint_json.py
@@ -7,28 +7,27 @@ def load_data(file_path):
     if not os.path.exists(file_path):
         return None
     with open(file_path, 'r') as file_handler:
-        json_code = json.load(file_handler)
-        return json_code
+        decoded_data = json.load(file_handler)
+        return decoded_data
 
 
-def pretty_print_json(json_code):
-    json_readable_user = json.dumps(json_code, ensure_ascii=False, indent=4)
-    return json_readable_user
+def pretty_print_json(decoded_data):
+    encoded_data = json.dumps(decoded_data, ensure_ascii=False, indent=4)
+    return encoded_data
 
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', type=str, help="command - input file")
+    parser.add_argument('-f', '--file',
+                        required=True,
+                        help="command - input file")
     return parser
 
 
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
-    if args.file is None:
-        print("no Data for work")
-    else:
-        try:
-            print(pretty_print_json(load_data(args.file)))
-        except ValueError:
-            print("not correct format")
+    try:
+        print(pretty_print_json(load_data(args.file)))
+    except ValueError:
+        print("not correct format")
